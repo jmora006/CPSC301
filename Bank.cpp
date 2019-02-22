bb774893b3803003d8e1f7a7648e92a0868c4028
Jason Mora-Mendoza <jasonmoramendoza@gmail.com>
2019-02-21 21:25:10 -0800

    Update Bank.cpp

--- Bank.cpp
+++ Bank.cpp
@@ -15,70 +15,69 @@ struct PERSON
 void Display(PERSON* a, int N)
 {
 	cout<<setprecision(2)<<fixed;
-	cout<<"Name\tBalance"<<endl; 
-  	cout<<"------------"<<endl; 
-  	for(int i = 0; i < N; i++)
-  	{
-  		cout<<a[i].Name<<" "<<a[i].Balance<<endl;
-  	}
+	cout<<"Name\tBalance"<<endl;
+	cout<<"------------"<<endl;
+	for(int i = 0; i < N; i++)
+	{
+		cout<<a[i].Name<<" "<<a[i].Balance<<endl;
+	}
 }
 
 void FindRichest(PERSON* a, int N)
-{ 
-	int max = 0; 
+{
+	int max = 0;
 	for(int i = 0; i < N; i++)
 	{
-    	if(a[max].Balance < a[i].Balance)
-    	{
-    		max = i; 
-    	}
-    }
-    cout<<endl;
-  	cout<<"The customer with maximum balance is: "<<a[max].Name<<endl; 
+		if(a[max].Balance < a[i].Balance)
+		{
+			max = i; 
+		}
+	}
+	cout<<endl;
+	cout<<"The customer with maximum balance is: "<<a[max].Name<<endl; 
 }
 
 void Deposit(char customerName[], PERSON* a, int N)
 {
-  	int wrong = -1; 
-  	for(int i = 0; i < N; i++) 
-  	{
-    	if (strcmp(customerName, a[i].Name) == 0)
-    	{
-    		wrong = i;
-    		break;
-    	}
-  	}
-  	
-  	if(wrong == -1)
-  	{
-  		cout<<"Wrong username. "<<endl;
-  	}
-  	
-  	else
-  	{
-  		double amount;
-    	cout<<customerName<<", how much would you like to deposit? "; 
-    	cin>>amount; 
-    	a[wrong].Balance += amount;
-    	cout<<"Now your new balance is "<<a[wrong].Balance<<endl; 
-  	}
+	int wrong = -1;
+	for(int i = 0; i < N; i++)
+	{
+		if (strcmp(customerName, a[i].Name) == 0)
+		{
+			wrong = i;
+			break;
+		}
+	}
+	
+	if(wrong == -1)
+	{
+		cout<<"Wrong username. "<<endl;
+	}
+	
+	else
+	{
+		double amount;
+		cout<<customerName<<", how much would you like to deposit? ";
+		cin>>amount;
+		a[wrong].Balance += amount;
+		cout<<"Now your new balance is "<<a[wrong].Balance<<endl;
+	}
 }
 
 void NewCopy(string firstName, PERSON* a, int N)
 {
 	ofstream out;
-    out.open(firstName.c_str());
-
-    for (int i = 0; i < N; i++) 
-    {
-        out<<a[i].Name<<" "<<a[i].Balance<<endl;
-    }
-
-    out.close();
+	out.open(firstName.c_str());
+	for (int i = 0; i < N; i++)
+	{
+		out<<a[i].Name<<" "<<a[i].Balance<<endl;
+	}
+	
+	out.close();
 }
 
 int main()
-{ 
+{
 	int N = 0;
 	ifstream file;
 	file.open("data.txt");
